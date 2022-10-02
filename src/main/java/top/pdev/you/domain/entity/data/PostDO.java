ef3aba479a90b8be9d41af203894355ce03e5942
peter <peter13034@outlook.com>
2022-10-02 13:04:45 +0800

    添加 帖子 DO

---
+++ src/main/java/top/pdev/you/domain/entity/data/PostDO.java
@@ -0,0 +1,39 @@
+package top.pdev.you.domain.entity.data;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 帖子
+ * Created in 2022/10/1 14:23
+ *
+ * @author Peter1303
+ */
+@TableName("post")
+@Data
+public class PostDO {
+    /**
+     * ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 社团 ID
+     */
+    private Long associationId;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 时间
+     */
+    private LocalDateTime time;
+}
