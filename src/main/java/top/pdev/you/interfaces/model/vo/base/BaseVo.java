477abe542e08df5c51fe6c9048905a3437ce9a9c
peter <peter13034@outlook.com>
2022-10-02 20:53:12 +0800

    添加 基本 VO

---
+++ src/main/java/top/pdev/you/interfaces/model/vo/base/BaseVo.java
@@ -0,0 +1,13 @@
+package top.pdev.you.interfaces.model.vo.base;
+
+import lombok.Data;
+
+/**
+ * 基本 VO
+ * Created in 2022/10/2 18:08
+ *
+ * @author Peter1303
+ */
+@Data
+public class BaseVo {
+}
