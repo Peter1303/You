d2dfd56a3bf15cf3e59497ad3917d788d841994a
peter <peter13034@outlook.com>
2022-10-02 13:08:31 +0800

    添加 初始用户领域实体

---
+++ src/main/java/top/pdev/you/domain/entity/User.java
@@ -0,0 +1,14 @@
+package top.pdev.you.domain.entity;
+
+import lombok.Data;
+import top.pdev.you.domain.entity.base.BaseEntity;
+
+/**
+ * 用户领域
+ * Created in 2022/9/30 22:32
+ *
+ * @author Peter1303
+ */
+@Data
+public class User extends BaseEntity {
+}
