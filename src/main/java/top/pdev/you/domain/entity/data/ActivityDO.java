b51b574c2f28b546d8d624f4a68f5d95756de028
peter <peter13034@outlook.com>
2022-10-02 13:05:46 +0800

    添加 活动 DO

---
+++ src/main/java/top/pdev/you/domain/entity/data/ActivityDO.java
@@ -0,0 +1,49 @@
+package top.pdev.you.domain.entity.data;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 活动
+ * Created in 2022/10/1 15:24
+ *
+ * @author Peter1303
+ */
+@TableName("activity")
+@Data
+public class ActivityDO {
+    /**
+     * ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 简介
+     */
+    private String summary;
+
+    /**
+     * 位置
+     */
+    private String location;
+
+    /**
+     * 活动时间
+     */
+    private LocalDateTime time;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+}
