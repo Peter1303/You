8be3e71347749978dc6d4dd2b426ccea4d6e231e
peter <peter13034@outlook.com>
2022-10-02 13:00:30 +0800

    添加 用户 DO

---
+++ src/main/java/top/pdev/you/domain/entity/data/UserDO.java
@@ -0,0 +1,44 @@
+package top.pdev.you.domain.entity.data;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 用户
+ * Created in 2022/9/30 22:34
+ *
+ * @author Peter1303
+ */
+@TableName("user")
+@Data
+public class UserDO {
+    /**
+     * ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 微信 ID
+     */
+    private String wechatId;
+
+    /**
+     * 目标 ID
+     */
+    private Long targetId;
+
+    /**
+     * 角色
+     */
+    private Integer role;
+
+    /**
+     * 时间
+     */
+    private LocalDateTime time;
+}
