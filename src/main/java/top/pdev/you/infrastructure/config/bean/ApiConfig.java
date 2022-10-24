e3d2324167c81cf55d76f3f188310652d1fca05e
peter <peter13034@outlook.com>
2022-10-24 13:55:27 +0800

    更新 配置类

---
+++ src/main/java/top/pdev/you/infrastructure/config/bean/ApiConfig.java
@@ -0,0 +1,57 @@
+package top.pdev.you.infrastructure.config.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import top.pdev.you.common.interceptor.TokenInterceptor;
+import top.pdev.you.infrastructure.resover.CurrentUserMethodArgumentResolver;
+
+import java.util.List;
+
+/**
+ * 配置
+ * Created in 2022/4/19 18:38
+ *
+ * @author Peter1303
+ */
+@EnableWebMvc
+@Configuration
+public class ApiConfig implements WebMvcConfigurer {
+
+    /**
+     * @return 令牌检查拦截器
+     */
+    @Bean
+    public TokenInterceptor getTokenInterceptor() {
+        return new TokenInterceptor();
+    }
+
+    /**
+     * 拦截器
+     *
+     * @param registry 注册
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getTokenInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/user/register/**",
+                        "/user/login"
+                );
+    }
+
+    /**
+     * 添加参数解析器
+     *
+     * @param resolvers 解析器
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        // 当前用户参数解析器
+        resolvers.add(new CurrentUserMethodArgumentResolver());
+    }
+}
--- src/main/java/top/pdev/you/infrastructure/config/bean/YouConfig.java
+++ src/main/java/top/pdev/you/infrastructure/config/bean/YouConfig.java
@@ -5,14 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import top.pdev.you.common.interceptor.TokenInterceptor;
-import top.pdev.you.infrastructure.resover.CurrentUserMethodArgumentResolver;
 import top.pdev.you.infrastructure.util.SpringEnvHelper;
-
-import java.util.List;
 
 /**
  * 配置
@@ -21,26 +14,10 @@ import java.util.List;
  * @author Peter1303
  */
 @Configuration
-public class YouConfig implements WebMvcConfigurer {
+public class YouConfig {
     @Bean
     public SpringEnvHelper getSpringEnvHelper() {
         return new SpringEnvHelper();
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 设置允许跨域的路径
-        registry.addMapping("/**")
-                // 设置允许跨域请求的域名
-                .allowedOriginPatterns("*")
-                // 是否允许 cookie
-                .allowCredentials(true)
-                // 设置允许的请求方式
-                .allowedMethods("GET", "POST", "DELETE", "PUT")
-                // 设置允许的 header 属性
-                .allowedHeaders("*")
-                // 跨域允许时间
-                .maxAge(3600);
     }
 
     /**
@@ -53,24 +30,5 @@ public class YouConfig implements WebMvcConfigurer {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
-    }
-
-    /**
-     * @return 令牌检查拦截器
-     */
-    @Bean
-    public TokenInterceptor getTokenInterceptor() {
-        return new TokenInterceptor();
-    }
-
-    /**
-     * 添加参数解析器
-     *
-     * @param resolvers 解析器
-     */
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        // 当前用户参数解析器
-        resolvers.add(new CurrentUserMethodArgumentResolver());
     }
 }
