a873a03a2027716360bab6645fb986c456afaae8
peter <peter13034@outlook.com>
2022-10-26 13:06:42 +0800

    加入 学生检查

--- src/main/java/top/pdev/you/domain/entity/base/BaseEntity.java
+++ src/main/java/top/pdev/you/domain/entity/base/BaseEntity.java
@@ -1,5 +1,10 @@
 package top.pdev.you.domain.entity.base;
 
+import top.pdev.you.common.exception.InternalErrorException;
+import top.pdev.you.domain.entity.Student;
+
+import java.util.Optional;
+
 /**
  * 基本实体
  * Created in 2022/9/30 22:32
@@ -7,4 +12,15 @@ package top.pdev.you.domain.entity.base;
  * @author Peter1303
  */
 public abstract class BaseEntity {
+    /**
+     * 检查学生
+     *
+     * @param student 学生
+     */
+    protected void checkStudent(Student student) {
+        Optional.ofNullable(student).orElseThrow(() -> new InternalErrorException("学生为空"));
+        Optional.ofNullable(student.getUser()).orElseThrow(() -> new InternalErrorException("用户为空"));
+        Optional.ofNullable(student.getUser().getUserId())
+                .orElseThrow(() -> new InternalErrorException("用户 ID 为空"));
+    }
 }
