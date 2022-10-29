2f69d81d317574bd534c4ed4669f1ee5659bc5c5
peter <peter13034@outlook.com>
2022-10-29 13:54:19 +0800

    增加 基类仓库

---
+++ src/main/java/top/pdev/you/domain/repository/base/BaseRepository.java
@@ -0,0 +1,26 @@
+package top.pdev.you.domain.repository.base;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import top.pdev.you.common.exception.InternalErrorException;
+import top.pdev.you.domain.entity.types.Id;
+
+import java.util.Optional;
+
+/**
+ * 基类仓库
+ * Created in 2022/10/29 13:48
+ *
+ * @author Peter1303
+ */
+public class BaseRepository<M extends BaseMapper<T>, T>
+        extends ServiceImpl<M, T> {
+    /**
+     * 检查 ID
+     *
+     * @param id ID
+     */
+    protected void checkId(Id id) {
+        Optional.ofNullable(id).orElseThrow(() -> new InternalErrorException("ID 为空"));
+    }
+}
