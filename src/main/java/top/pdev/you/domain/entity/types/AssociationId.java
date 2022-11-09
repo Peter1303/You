d587d481b54ebde74465b1ceb268f37c01724dbc
peter <peter13034@outlook.com>
2022-11-09 13:17:32 +0800

    修改 继承关系

--- src/main/java/top/pdev/you/domain/entity/types/AssociationId.java
+++ src/main/java/top/pdev/you/domain/entity/types/AssociationId.java
@@ -1,18 +1,13 @@
 package top.pdev.you.domain.entity.types;
 
-import lombok.Data;
-
 /**
  * 社团 ID
  * Created in 2022/10/1 16:46
  *
  * @author Peter1303
  */
-@Data
-public class AssociationId {
-    private Long id;
-
+public class AssociationId extends Id {
     public AssociationId(Long id) {
-        this.id = id;
+        super(id);
     }
 }
