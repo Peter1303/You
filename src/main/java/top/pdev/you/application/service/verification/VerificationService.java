42984cd6732c85e0025e6c64a7d3fe8ae271c092
peter <peter13034@outlook.com>
2023-04-24 20:41:27 +0800

    添加 认证服务

---
+++ src/main/java/top/pdev/you/application/service/verification/VerificationService.java
@@ -0,0 +1,16 @@
+package top.pdev.you.application.service.verification;
+
+import top.pdev.you.common.entity.TokenInfo;
+import top.pdev.you.domain.entity.User;
+
+/**
+ * 认证服务
+ * Created in 2023/4/24 20:36
+ *
+ * @author Peter1303
+ */
+public interface VerificationService {
+    TokenInfo getTokenInfo();
+
+    User currentUser();
+}
